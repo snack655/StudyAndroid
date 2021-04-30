23180b840d538701047ab95d7dfb1826ab288a87
snack655 <80940200+snack655@users.noreply.github.com>
2021-04-30 22:16:02 +0900

    Layout inflation
    Turn layout.
    ...?

---
+++ SampleLayoutInflater/app/src/main/java/com/example/samplelayoutinflater/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.samplelayoutinflater;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.example.samplelayoutinflater.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "버튼이 눌렸어요.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
