b960eaf2d6ce06365e25462bfa0089157c6bf97a
snack655 <cmjcmj1230@gmail.com>
2021-07-16 23:46:34 +0900

    2021-07-16 broadcast
    broadcast!

--- SampleService/app/src/main/java/org/techtown/service/MainActivity.java
+++ SampleService/app/src/main/java/org/techtown/service/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText editText;
@@ -32,5 +33,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Intent passedIntent = getIntent();
+        processIntent(passedIntent);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+
+        processIntent(intent);
+
+        super.onNewIntent(intent);
+    }
+
+    private void processIntent(Intent intent) {
+        if(intent != null) {
+            String command = intent.getStringExtra("command");
+            String name = intent.getStringExtra("name");
+
+            Toast.makeText(this, "command : "+command+", name :" +name, Toast.LENGTH_SHORT).show();
+        }
     }
 }
