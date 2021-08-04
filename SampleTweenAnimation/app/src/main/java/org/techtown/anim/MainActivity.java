d9c8aaaad215bc03c0e2c4f247976d529a5bd634
snack655 <cmjcmj1230@gmail.com>
2021-08-05 03:40:25 +0900

    2021-08-05 Layout and Animation
    Study Android
    -> Layout and Animation!

---
+++ SampleTweenAnimation/app/src/main/java/org/techtown/anim/MainActivity.java
@@ -0,0 +1,40 @@
+package org.techtown.anim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Animation anim =
+                        AnimationUtils.loadAnimation(getApplicationContext(),R.anim.alpha);
+
+                v.startAnimation(anim);
+            }
+        });
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Animation anim =
+                        AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate);
+
+                v.startAnimation(anim);
+            }
+        });
+    }
+}
