99c132f0ff78c2a4b5e10fba21081a996cc983db
snack655 <cmjcmj1230@gmail.com>
2021-08-13 18:39:52 +0900

    Study Android Thread Java
    2021-08-13
    Tread - 1

---
+++ SampleThread/app/src/main/java/org/techtown/sampletread/MainActivity.java
@@ -0,0 +1,47 @@
+package org.techtown.sampletread;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int value = 0;
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BackgroundTread thread = new BackgroundTread();
+                thread.start();
+            }
+        });
+    }
+
+    class BackgroundTread extends Thread {
+        public void run() {
+            for(int i = 0; i < 100; i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch(Exception e) {}
+
+                value += 1;
+                Log.d("Tread", "value : "+value);
+                textView.setText("value 값 : "+value);
+            }
+        }
+    }
+}
+
